7306b405e449bd5bd7bf32900b2223db8c6ede53
waleed reda <waleed.reda@fawry.com>
2023-12-17 00:28:07 +0200

    handle app response

---
+++ src/main/java/com/waleedreda/core/common/AppResponse.java
@@ -0,0 +1,27 @@
+package com.waleedreda.core.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppResponse <T>{
+    private StatusEnum status;
+    private T data;
+    private String errorCode;
+    private String errorMessage;
+
+    @Override
+    public String toString() {
+        return "AppResponse{" +
+                "status=" + status +
+                ", data=" + data +
+                ", errorCode='" + errorCode + '\'' +
+                ", errorMessage='" + errorMessage + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/waleedreda/core/common/StatusEnum.java
@@ -0,0 +1,9 @@
+package com.waleedreda.core.common;
+
+public enum StatusEnum {
+    SUCCESS,
+    FAILED;
+
+    private StatusEnum() {
+    }
+}
